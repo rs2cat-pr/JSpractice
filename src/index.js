8b25a6ed3c5b6e0ba1121560c4cf600a63dd5ef2
rs2cat-pr <93893048+rs2cat-pr@users.noreply.github.com>
2021-11-13 12:05:09 +0900

    map,filterの使い方

--- src/index.js
+++ src/index.js
@@ -65,7 +65,77 @@ const profile3 = `私の名前は${name2}で年齢は${age2}です`;
 /**
  * デフォルト値、引数
  */
-const sayHello = (user = "名前未入力さん") => console.log(`こんにちは${user}`);
-sayHello();
-
 //変数に名前がないとundefinedになる
+//引数にデフォルト値を追加
+const sayHello = (user = "名前未入力さん") => console.log(`こんにちは${user}`);
+//sayHello();
+
+/**
+ * スプレッド構文 ...
+ * 配列とかで使用できる
+ */
+//* 配列
+const arr1 = [1, 2];
+//配列の中身を順番に展開してくれる
+// (any,any,...)の形
+//console.log(...arr1);
+
+const sumFunc1 = (num1, num2) => {
+  return console.log(num1 + num2);
+};
+//sumFunc1(...arr1);
+
+//* 配列の分割代入
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+//分割代入と組み合わせて一部を再配列化することができる
+//console.log(num1)
+//console.log(arr3)
+
+//* 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+const arr7 = [...arr6, ...arr5];
+//console.log(arr7)
+
+// arr6 = arr4を行うと、arr6[]をいじった時にarr4まで変更される
+// 参照元と依存関係を断つためにスプレッド構文を用いる
+
+/**
+ * map・filterを使用した配列の処理
+ */
+/**　map 配列を展開*/
+// const nameArr1 = ["田中", "鈴木", "山田"];
+// //従来　for文を使った展開
+// for (let index = 0; index < nameArr1.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr1[index]}`);
+// }
+
+// //mapは配列に対して使用できる関数
+// //mapを使用してnameArr２にnameArr1の値を再配列化
+// const nameArr2 = nameArr1.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2)
+//配列の内容を展開して、出力
+// nameArr2.map((name, index) => console.log(`${index + 1}は${name}`));
+
+// /**　filter 検索条件を指定して条件に一致する配列を展開*/
+// const numArr = [1, 2, 3, 4, 5];
+// const numArr2 = numArr.filter((num) => {
+//   //returnに条件式を書く
+//   return num % 2 === 1;
+// });
+// console.log(numArr2);
+
+// const nameArr3 = nameArr1.map((name) => {
+//   if (name === "山田") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(nameArr3);
